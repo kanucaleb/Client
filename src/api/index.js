7f7bfd9bee9088043f1199812fb2ca39ad771048
Caleb <kanucaleb5@gmail.com>
2023-05-31 18:54:24 +0100

    fourth commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const url = process.env.REACT_APP_BACKEND_URL;
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
-export const updatePost = (id, updatedPost)=> axios.patch(`${url}/${id}`, updatedPost);
-export const deletePost = (id)=> axios.delete(`${url}/${id}`);
-export const likePost = (id)=> axios.patch(`${url}/${id}/likePost`);
+const API_URL = `${url}/posts/`;
+
+export const fetchPosts = () => axios.get(API_URL);
+export const createPost = (newPost) => axios.post(API_URL, newPost);
+export const updatePost = (id, updatedPost)=> axios.patch(API_URL + id, updatedPost);
+export const deletePost = (id)=> axios.delete(API_URL + id);
+export const likePost = (id)=> axios.patch(API_URL + id + likePost);
