417697bf0d363d4bce06ba1b6c7dda8ee7c957ce
Caleb <kanucaleb5@gmail.com>
2023-05-31 19:28:51 +0100

    fifth commit

--- src/api/index.js
+++ src/api/index.js
@@ -8,4 +8,4 @@ export const fetchPosts = () => axios.get(API_URL);
 export const createPost = (newPost) => axios.post(API_URL, newPost);
 export const updatePost = (id, updatedPost)=> axios.patch(API_URL + id, updatedPost);
 export const deletePost = (id)=> axios.delete(API_URL + id);
-export const likePost = (id)=> axios.patch(API_URL + id + likePost);
+export const likePost = (id)=> axios.patch(API_URL + id + '/likePost');
